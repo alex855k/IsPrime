bb734ce18899e36996b4df29ad01e232aa827de7
alhe2 <alhe@eal.dk>
2017-02-06 13:36:35 +0100

    Added threads to make it more responsive

---
+++ IsPrimeSingleThreaded/PrimeChecker.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace IsPrimeSingleThreaded
+{
+    class PrimeChecker
+    {
+        private long number;
+        private Stopwatch stopwatch = new Stopwatch();
+
+        public PrimeChecker(long input)
+        {
+            this.number = input;
+        }
+
+        public void Check()
+        {
+            stopwatch.Start();
+            try
+            {
+                long result = IsPrime();
+
+                string output;
+                if (result == 0)
+                {
+                    output = "is prime!";
+                }
+                else
+                {
+                    output = "is NOT prime, contains factor " + result;
+                }
+                Console.WriteLine($"{number} " + output);
+                Console.WriteLine("Time (ms) : " + stopwatch.Elapsed);
+                stopwatch.Reset();
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Invalid parameter! " + e.Message);
+            }
+        }
+
+        #region Check if number is a prime
+        //returns 0 if number is prime, orherwise the first factor of number
+        public long IsPrime()
+        {
+            int delayTime = 25;
+            if (number <= 1) throw new ArgumentOutOfRangeException(); //primes must be larger than 1
+            long result;
+            if (number % 2 == 0)
+            {//number is even
+                if (number == 2)
+                {
+                    result = 0; //2 is the only even prime
+                }
+                else
+                {
+                    result = 2; //other even numbers are not prime as 2 is a factor
+                }
+            }
+            else
+            {
+                long ceiling = (long)Math.Sqrt(number);
+                long i = 3;
+                while ((i <= ceiling) && (number % i != 0))
+                {
+                    Thread.Sleep(delayTime);//simulate hard calculation
+                    i = i + 2; // only check uneven factors, we have testet even 
+                }
+                if ((number % i == 0) && (number != i))
+                {
+                    result = i; // i is a factor
+                }
+                else
+                {
+                    result = 0; // number is prime
+                }
+            }
+            stopwatch.Stop();
+            return result;
+        }
+        #endregion
+    }
+}
--- IsPrimeSingleThreaded/Program.cs
+++ IsPrimeSingleThreaded/Program.cs
@@ -1,50 +1,46 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Threading;
 
 namespace IsPrimeSingleThreaded
 {
     class Program
     {
-        private Stopwatch stopwatch = new Stopwatch();
         bool goOn = true;
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             Program program = new IsPrimeSingleThreaded.Program();
             program.Run();
         }
 
-        public void Run() {
-            do {
+        public void Run()
+        {
+            do
+            {
                 long input = GetUserValue();
-                string output;
-                if (input == 0) {
+                if (input == 0)
+                {
                     goOn = false;
                 }
-                else {
-                    try {
-                        stopwatch.Start();
-                        long result = Utils.isPrime(input);
-                        if (result == 0) {
-                            output = "is prime!";
-                        }
-                        else {
-                            output = "is NOT prime, contains factor " + result;
-                        }
-                        Console.WriteLine($"{input} " + output);
-                        stopwatch.Stop();
-                        Console.WriteLine("Time (ms) : " + stopwatch.Elapsed);
-                        stopwatch.Reset();
-                    }
-                    catch (ArgumentOutOfRangeException e) {
-                        Console.WriteLine("Nonvalid parameter! " + e.ToString());
-                    }
+                else
+                {
+                    PrimeChecker pc = new PrimeChecker(input);
+                    new Thread(pc.Check).Start();
                 }
             } while (goOn);
         }
 
-        private long GetUserValue() {
+        private long GetUserValue()
+        {
             Console.Write("Enter a positive integer value to test whether it is prime (enter 0 to exit) : ");
-            return long.Parse(Console.ReadLine());
+            try
+            {
+                return long.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                return 1;
+            }
         }
     }
 }
